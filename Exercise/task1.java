63b8444cc49c59811f369ac9fbfd834fa76f5606
Vu Dang Hoang <vdhoang240706@gmail.com>
2025-08-25 14:57:45 +0300

    commit

--- Exercise/task1.java
+++ Exercise/task1.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 public class task1 {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("How many random names the program should generate?");
+        int n = Integer.parseInt(reader.nextLine());
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
