39f86f82dd28656bd3fa444430dc3b6efe2aab37
Vu Dang Hoang <vdhoang240706@gmail.com>
2025-08-25 14:57:52 +0300

    commit

--- Exercise/task1.java
+++ Exercise/task1.java
@@ -6,10 +6,13 @@ public class task1 {
         System.out.println("How many random names the program should generate?");
         int n = Integer.parseInt(reader.nextLine());
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String[] lastname = {"A", "B", "C", "D", "E", "F" };
+        String[] firstname = {"G", "H", "I", "K", "L", "M" };
+
+        for (int i = 0; i < n; i++) {
+            int luku = (int) (Math.random() * lastname.length);
+            int luku2 = (int) (Math.random() * firstname.length);
+            System.out.println(lastname[luku] + " " + firstname[luku2]);
         }
     }
 }
