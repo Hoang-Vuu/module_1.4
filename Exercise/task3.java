b5845dd316bb7d1063b2044f4b2be6cdf972748d
Vu Dang Hoang <vdhoang240706@gmail.com>
2025-08-25 14:55:07 +0300

    Initial commit

---
+++ Exercise/task3.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class demo {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int n = reader.nextInt();
+        int[] numbers = new int[n];
+
+        System.out.println("Enter the integers into the array:");
+        int i;
+        for (i = 0; i < n; i++) {
+            System.out.print("Enter integer " + (i + 1) + ": ");
+            numbers[i] = reader.nextInt();
+        }
+
+        int[] unique = new int[n];
+        int count = 0;
+
+        for (int a = 0; a < n; a++) {
+            int duplicateCount = 0;
+            for (int j = 0; j < count; j++) {
+                if (numbers[a] == unique[j]) {
+                    duplicateCount++;
+                }
+            }
+            if (duplicateCount == 0) {
+                unique[count] = numbers[a];
+                count++;
+            }
+        }
+
+        System.out.print("Array after removing duplicates: ");
+        for (int b = 0; b < count; b++) {
+            System.out.print(unique[b] + " ");
+        }
+        System.out.println();
+    }
+}
